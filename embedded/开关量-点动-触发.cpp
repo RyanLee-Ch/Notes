a008e88c9cd015b1b6eb665a780790b141db4ccc
RyanLee-Ch <ryanlee990929@gmail.com>
2024-07-29 02:24:19 +0800

    Update 开关量-点动-触发.cpp

--- embedded/开关量-点动-触发.cpp
+++ embedded/开关量-点动-触发.cpp
@@ -1,15 +1,19 @@
 void setup() {
-  pinMode(1, INPUT_PULLUP);  // 设置 1口 为输入
-  pinMode(2, OUTPUT);  // 设置 2口 为输出
-  digitalWrite(2, LOW);  // 设置 2口 默认为低电平0
+  initializeButton(1, 2);  // 定义初始化情况：1入 2出
 }
 void loop() {
-  controlRelay(1, 2);  // 调用函数，1口入, 2口点动执行
-  delay(100);  // 抖动防止
+  controlRelay(1, 2);  // 调用触发函数，1入, 2点动执行
 }
-// 定义触发函数
-void controlRelay(int buttonPin, int relayPin) {
-  if (digitalRead(buttonPin) == HIGH) {
+
+// 点动函数
+void initializeButton(int inputPort, int outputPort) {  // 定义初始化情况
+  pinMode(inputPort, INPUT_PULLUP);  // 设置 1口 为输入
+  pinMode(outputPort, OUTPUT);  // 设置 2口 为输出
+  digitalWrite(2, LOW);  // 设置 2口 默认为低电平0
+}
+void controlRelay(int buttonPin, int relayPin) {  // 定义触发函数
+  delay(100);  // 抖动防止
+  if (digitalRead(buttonPin) == HIGH) {  // 检测按钮摁下则点动
     digitalWrite(relayPin, HIGH);
   } else {
     digitalWrite(relayPin, LOW);
