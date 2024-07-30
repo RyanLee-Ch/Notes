56ed63edd90801e9e698e6fa819c07381bb18da4
RyanLee-Ch <ryanlee990929@gmail.com>
2024-07-30 17:08:06 +0800

    Create 连接-wifi-获取-ip.cpp

---
+++ embedded/连接-wifi-获取-ip.cpp
@@ -0,0 +1,34 @@
+#include <WiFi.h>  // 导入wifi库
+
+const char* ssid = "Celitron";  // wifi名称
+const char* password = "Celitron2022";  // wifi密码
+
+void setup() {
+  connWifi();
+}
+
+void loop() {
+  // nothing to do
+}
+
+void connWifi() {
+  Serial.begin(115200);
+  delay(10);
+
+  Serial.println();
+  Serial.println();
+  Serial.print("Connecting to ");
+  Serial.println(ssid);
+
+  WiFi.begin(ssid, password);
+
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(1000);
+    Serial.print(".");
+  }
+
+  Serial.println("");
+  Serial.println("WiFi connected.");
+  Serial.print("IP address: ");
+  Serial.println(WiFi.localIP());
+}
