a4730d39339e2c664b139b072d4c6a68d904fd55
RyanLee-Ch <ryanlee990929@gmail.com>
2024-07-30 22:35:01 +0800

    Update 连接-wifi-获取-ip.cpp

--- embedded/连接-wifi-获取-ip.cpp
+++ embedded/连接-wifi-获取-ip.cpp
@@ -4,6 +4,7 @@ const char* ssid = "小宝贝";  // wifi名称
 const char* password = "lyj3112424";  // wifi密码
 
 void setup() {
+  Serial.begin(115200);  // 连接检测器，可以在检测器中查看ip，然后通过http://ip地址访问，配合web服务应用
   connWifi();
 }
 
@@ -12,19 +13,12 @@ void loop() {
 
 // wifi函数
 void connWifi() {
-  Serial.begin(115200);  // 连接检测器，可以在检测器中查看ip，然后通过http://ip地址访问，配合web服务应用
   delay(10);
   Serial.println();
   Serial.println();
-  Serial.print("Connecting to ");
-  Serial.println(ssid);
-  WiFi.begin(ssid, password);
-  while (WiFi.status() != WL_CONNECTED) {
-    delay(1000);
-    Serial.print(".");
-  }
-  Serial.println("");
-  Serial.println("WiFi connected.");
-  Serial.print("IP address: ");
+  Serial.print("Connecting to " + String(ssid) + "：");
+  WiFi.begin(ssid, password);  // 开始连接WiFi
+  while (WiFi.status() != WL_CONNECTED) {delay(1000);Serial.print("·");}
+  Serial.print("\nWiFi connected, IP address: ");
   Serial.println(WiFi.localIP());
 }
