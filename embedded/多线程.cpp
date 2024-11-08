0dd92d21a3330d5499c2c31396ee3a6ce5d74019
Ryan <ryanlee990929@gmail.com>
2024-11-08 08:53:20 +0800

    Update 多线程.cpp
    修改内存查看条件

--- embedded/多线程.cpp
+++ embedded/多线程.cpp
@@ -15,26 +15,17 @@ void initializeButton(int inputPort, int outputPort) {
 void controlRelay(int buttonPin, int relayPin) {
   if (digitalRead(buttonPin) == LOW) {
     digitalWrite(relayPin, HIGH);
-    printMemoryInfo(__FUNCTION__);  // 调用内存信息打印函数（放在函数主要资源消耗的位置）
   } else {
     digitalWrite(relayPin, LOW);
   }
 }
 
-// 内存信息打印函数（直接copy）
+// 内存信息打印函数（直接copy），触发后即可显示当前任务的内存使用量，以及系统总内存
 void printMemoryInfo(const char* functionName) {
   // 检查当前任务的栈高水位标记
-  UBaseType_t stackUsage = uxTaskGetStackHighWaterMark(NULL);
-  Serial.print(functionName);
-  Serial.print(" Stack high water mark: ");
-  Serial.println(stackUsage);
+  Serial.print(String(functionName) + " Stack high water mark: " + String(uxTaskGetStackHighWaterMark(NULL)));
   // 打印系统的总内存和剩余内存
-  size_t totalMemory = ESP.getHeapSize();
-  size_t freeMemory = ESP.getFreeHeap();
-  Serial.print("Total heap memory: ");
-  Serial.println(totalMemory);
-  Serial.print("Free heap memory: ");
-  Serial.println(freeMemory);
+  Serial.println(". Total heap memory: " + String(ESP.getHeapSize()) + ", Free heap memory: " + String(ESP.getFreeHeap()));
 }
 
 // 多线程任务函数
@@ -43,6 +34,9 @@ void buttonTask(void *pvParameters) {  // 多线程setup、loop
   setUpButton();  // setup对象
   while (true) {
     loopButton();  // loop对象
+    if (digitalRead(1) == LOW) {  // 设置内存查看条件
+      printMemoryInfo(__FUNCTION__);  // 调用内存信息打印函数（放在函数主要资源消耗的位置）
+    }
     vTaskDelay(pdMS_TO_TICKS(100));  // LOOP短暂延时循环，避免任务占用过多资源
   }
 }
