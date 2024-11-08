1cb6abda5a93d7908db4e017cc0ce83122bcfb74
Ryan <ryanlee990929@gmail.com>
2024-11-08 08:36:40 +0800

    Update 内存查看.cpp

--- embedded/内存查看.cpp
+++ embedded/内存查看.cpp
@@ -14,17 +14,9 @@ void controlRelay(int buttonPin, int relayPin) {
 // 内存信息打印函数，触发后即可显示当前任务的内存使用量，以及系统总内存
 void printMemoryInfo(const char* functionName) {
   // 检查当前任务的栈高水位标记
-  UBaseType_t stackUsage = uxTaskGetStackHighWaterMark(NULL);
-  Serial.print(functionName);
-  Serial.print(" Stack high water mark: ");
-  Serial.println(stackUsage);
+  Serial.println(String(functionName) + " Stack high water mark: " + String(uxTaskGetStackHighWaterMark(NULL)));
   // 打印系统的总内存和剩余内存
-  size_t totalMemory = ESP.getHeapSize();
-  size_t freeMemory = ESP.getFreeHeap();
-  Serial.print("Total heap memory: ");
-  Serial.println(totalMemory);
-  Serial.print("Free heap memory: ");
-  Serial.println(freeMemory);
+  Serial.println("Total heap memory: " + String(ESP.getHeapSize()) + "Free heap memory: " + String(ESP.getFreeHeap()));
 }
 
 void setup() {
